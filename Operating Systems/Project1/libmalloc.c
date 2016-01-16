3421e53da18f2a7bb4ff8932bf03e24e198b5112
dziku1337 <synmotopompy@gmail.com>
2016-01-17 00:32:10 +0100

    Update

--- Operating Systems/Project1/libmalloc.c
+++ Operating Systems/Project1/libmalloc.c
@@ -14,7 +14,7 @@
 #define NEW_AREA_THRESHOLD (1<<17)
 // Ile sumarycznie wolnego miejsca powinno być w obszarach, by
 // usunąć obszar
-#define MEM_TO_DESTROY_AREA 4*(1 << 17)
+#define DESTROY_AREA_CONDITION 4*(1 << 17)
 // Status bloku w dwukierunkowej liście
 #define TAKEN 1
 #define FREE 0
@@ -41,19 +41,22 @@ typedef struct area
     pthread_mutex_t mutex;
     struct area *next, *prev;
     blockHeader* first;
-    unsigned int size;
-    unsigned int totalSizeLeft;
+    size_t size;
+    size_t totalSizeLeft;
     unsigned int threadCount;
 
     char allignmentPadding[ALLIGN_UNIT - (sizeof(pthread_mutex_t) +
-                        4*sizeof(void*) +sizeof(unsigned int)) % ALLIGN_UNIT];
+                        3*sizeof(void*) + 2*sizeof(size_t) +
+                        sizeof(unsigned int)) % ALLIGN_UNIT];
 
 } area;
 
-pthread_mutex_t areaListMutex;
+pthread_mutex_t areaListMutex = PTHREAD_MUTEX_INITIALIZER;
+
 area *firstArea = NULL, *lastArea = NULL;
 unsigned int areaCount = 0;
 unsigned int totalSizeLeft = 0;
+
 
 void initializeArea(area* a, size_t size)
 {
@@ -83,7 +86,63 @@ area* createNewArea(size_t size)
     initializeArea(newArea, size);
 }
 
-void* allocateFreeBlock(blockHeader* header, size_t size)
+void destroyArea(area* toDestroy)
+{
+    pthread_mutex_destroy(toDestroy->mutex);
+    size_t size = toDestroy->size;
+
+    munmap(toDestroy, size);
+}
+
+void areaListAppend(area* a)
+{
+    pthread_mutex_lock(&areaListMutex);
+
+    if(areaCount == 0)
+    {
+        firstArea = a;
+        lastArea = a;
+    }
+    else
+    {
+        lastArea->next = a;
+        a->prev = lastArea;
+        lastArea = a;
+    }
+    __sync_add_and_fetch(areaCount, 1);
+
+    pthread_mutex_unlock(&areaListMutex);
+}
+
+void areaListDelete(area* a)
+{
+    pthread_mutex_lock(&areaListMutex);
+
+    area *prev = a->prev, *next = a->next;
+
+    if(prev != NULL)
+    {
+        prev->next = next;
+    }
+    else
+    {
+        firstArea = next;
+    }
+
+    if(next != NULL)
+    {
+        next->prev = prev;
+    }
+    else
+    {
+        lastArea = prev;
+    }
+
+    pthread_mutex_unlock(&areaListMutex);
+    __sync_sub_and_fetch(areaCount, 1);
+}
+
+void* allocateBlock(blockHeader* header, size_t size)
 {
     size += ALLIGN_UNIT - size % ALLIGN_UNIT;
 
@@ -110,6 +169,10 @@ void* allocateFreeBlock(blockHeader* header, size_t size)
         header->areaPtr->totalSizeLeft -= size + sizeof(blockHeader);
         header->length = size;
         // Podłączam bloki do siebie
+        if(header->next != NULL)
+        {
+            header->next->prev = newHeader;
+        }
         newHeader->next = header->next;
         header->next = newHeader;
         newHeader->prev = header;
@@ -126,8 +189,38 @@ void* allocateFreeBlock(blockHeader* header, size_t size)
 
 void freeBlock(blockHeader* header)
 {
+    area* ownerArea = header->areaPtr;
+
     header->type = FREE;
-    
+    ownerArea->totalSizeLeft += header->length;
+    blockHeader *prev = header->prev, *next = header->next;
+
+    if(next != NULL && next->type == FREE)
+    {
+        // Łączymy dalszy blok
+        if(next->next != NULL)
+        {
+            next->next->prev = header;
+        }
+        header->next = next->next;
+        header->length += sizeof(blockHeader) + next->length;
+
+        ownerArea->totalSizeLeft += sizeof(blockHeader);
+    }
+
+    if(prev != NULL && prev->type == FREE)
+    {
+        // Łączymy się z poprzednim blokiem
+        if(header->next != NULL)
+        {
+            header->next->prev = prev;
+        }
+        prev->next = header->next;
+        prev->length += sizeof(blockHeader) + header->length;
+
+        ownerArea->totalSizeLeft += sizeof(blockHeader);
+    }
+
 }
 
 
@@ -144,7 +237,7 @@ void* malloc(size_t size)
             if(pthread_mutex_trylock(currentArea->mutex) == 0)
             {
                 //Założyliśmy blokadę
-                void* ptr = allocateFreeBlock(area->first, size);
+                void* ptr = allocateBlock(area->first, size);
                 void* nextArea = currentArea->next;
                 pthread_mutex_unlock(currentArea->mutex);
             }
@@ -159,14 +252,14 @@ void* malloc(size_t size)
         }
     }
     // Skoro tutaj jestem, to albo żądany rozmiar jest duży, albo
-    // nie znalazłem wolnej areny/wolnego bloku.
+    // nie znalazłem wolnego obszaru/bloku.
     // Tworzę obszar z jednym zajętym blokiem i podpinam go do listy
 
     area* newArea = createNewArea(max(AREA_SIZE, size));
     pthread_mutex_lock(newArea->mutex);
-    newArea->threadCount = 1;
+    __sync_add_and_fetch(newArea->threadCount, 1);
 
-    void* ptr = allocateFreeBlock(newArea->first, size);
+    void* ptr = allocateBlock(newArea->first, size);
 
     areaListAppend(newArea);
     __sync_sub_and_fetch(newArea->threadCount, 1);
@@ -175,24 +268,106 @@ void* malloc(size_t size)
     return ptr;
 }
 
+void free(void* ptr)
+{
+    blockHeader* header = ptr - sizeof(blockHeader);
+    area* oldArea = header->areaPtr;
+
+    __sync_add_and_fetch(oldArea->threadCount, 1);
+    pthread_mutex_lock(oldArea->mutex);
+    freeBlock(header);
+
+    // Czy obszar składa się z jednego wolnego bloku?
+    if(oldArea->first->length < oldArea->totalSizeLeft
+        || oldArea->first->type == TAKEN)
+    {
+        __sync_sub_and_fetch(oldArea->threadCount, 1);
+        pthread_mutex_unlock(oldArea->mutex);
+        return;
+    }
+
+    // Czy wolna pamięć w pozostałych obszarach przekracza ustalony próg?
+    unsigned int freeMemory = 0;
+    area* currentArea = firstArea;
+
+    while(currentArea != NULL)
+    {
+        if(currentArea != oldArea)
+        {
+            freeMemory += currentArea->totalSizeLeft;
+        }
+        currentArea = currentArea->next;
+    }
+
+    if(freeMemory >= DESTROY_AREA_CONDITION)
+    {
+        areaListDelete(oldArea);
+
+        while(oldArea->threadCount > 1)
+        {
+            // Czekaj
+        }
+        destroyArea(oldArea);
+    }
+    else
+    {
+        __sync_sub_and_fetch(oldArea->threadCount, 1);
+        pthread_mutex_unlock(oldArea->mutex);
+    }
+}
+
 void* calloc(size_t count, size_t size)
 {
-
+    void* ptr = malloc(count * size);
+    memset(ptr, 0, count * size);
+    return ptr;
 }
 
 void* realloc(void* ptr, size_t size)
 {
+    blockHeader* header = ptr - sizeof(blockHeader);
+    if(header->length > size) return NULL;
+    else if(header->length == size) return ptr;
 
-}
+    void* newPtr = malloc(size);
+    memcpy(newPtr, ptr, header->length);
+    free(ptr);
 
-void free(void* ptr)
-{
-
+    return newPtr;
 }
 
 void printMemoryState()
 {
+    printf("-----------------\n");
+    printf("STAN PAMIĘCI\n");
+    printf("-----------------\n");
+    area* currentArea = firstArea;
+    while(currentArea != NULL)
+    {
+        pthread_mutex_lock(currentArea->mutex);
+        printf("-----------------\n");
+        printf("Obszar %p, rozmiar %lu\n", currentArea, currentArea->)
+        printf("-----------------\n");
 
+        blockHeader* header = currentArea->first;
+        while(header != NULL)
+        {
+            if(header->type == FREE)
+            {
+                printf("F ");
+            }
+            else
+            {
+                printf("T ");
+            }
+            printf("%p, rozmiar %lu\n", header + sizeof(header), header->length);
+
+            header = header->next;
+        }
+
+        pthread_mutex_unlock(currentArea->mutex);
+        currentArea = currentArea->next;
+    }
 }
 
 int main()
