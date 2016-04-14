55c10144a3fe719a873dc8972c4b3af0d3925509
dziku1337 <synmotopompy@gmail.com>
2016-04-15 00:01:33 +0200

    Obj. Oriented Design Assignment 4 done, cya later

---
+++ Object Oriented Design/Zestaw 4/Zadanie4Tests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Lista_4
+{
+    [TestFixture]
+    class Zadanie4Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            TagBuilder tag = new TagBuilder();
+            string correctTag = 
+            @"<parent parentproperty1='true' parentproperty2='5'>
+    <child1 childproperty1='c'>
+        childbody
+    </child1>
+    <child2 childproperty2='c'>
+        childbody
+    </child2>
+</parent>
+<script>
+    $.scriptbody();
+</script>
+";
+            
+            tag.IsIndented = true;
+            tag.Indentation = 4;
+            string builtTag =
+                     tag.StartTag("parent")
+                         .AddAttribute("parentproperty1", "true")
+                         .AddAttribute("parentproperty2", "5")
+                             .StartTag("child1")
+                             .AddAttribute("childproperty1", "c")
+                             .AddContent("childbody")
+                             .EndTag()
+                             .StartTag("child2")
+                             .AddAttribute("childproperty2", "c")
+                             .AddContent("childbody")
+                             .EndTag()
+                         .EndTag()
+                         .StartTag("script")
+                         .AddContent("$.scriptbody();")
+                         .EndTag()
+                         .ToString();
+
+            Assert.AreEqual(builtTag, correctTag);
+        }
+    }
+}
