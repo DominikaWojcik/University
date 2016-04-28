1ad7f4fcf676e09c96acb04b2473683f7ef3ab96
dziku1337 <synmotopompy@gmail.com>
2016-04-28 22:01:15 +0200

    Object oriented design assignment 6

---
+++ Object Oriented Design/Zestaw 6/Tree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_6
+{
+    public abstract class Tree
+    {
+        public virtual void Accept(TreeVisitorUnaware visitor)
+        { }
+    }
+
+    public class TreeNode : Tree
+    {
+        public Tree Left { get; set; }
+        public Tree Right { get; set; }
+
+        public override void Accept(TreeVisitorUnaware visitor)
+        {
+            visitor.Visit(this);
+
+            if (Left != null)
+                Left.Accept(visitor);
+
+            if (Right != null)
+                Right.Accept(visitor);
+        }
+    }
+
+    public class TreeLeaf : Tree
+    {
+        public override void Accept(TreeVisitorUnaware visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+}
